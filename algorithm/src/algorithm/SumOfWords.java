bdb89b60bfb648bda90011db0a4d06b5150df76b
chenbo <chamber_work@163.com>
2017-02-21 17:24:18 +0800

    “algorithm

---
+++ algorithm/src/algorithm/SumOfWords.java
@@ -0,0 +1,54 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class SumOfWords {
+
+	public static Map<Character,Integer> countChars(String str){
+		Map<Character,Integer> map = new HashMap<Character, Integer>();
+		for(int i=0;i<str.length();i++){
+			char c = str.charAt(i);
+			if(map.containsKey(c)){
+				int temp = map.get(c)+1;
+				map.put(c, temp);
+			}else{
+				map.put(c, 1);
+			}
+		}
+		return map;
+		
+	}
+	public static void SortMap(Map<Character,Integer>  map){
+		
+		List<Map.Entry<Character,Integer>> listData = new ArrayList<Map.Entry<Character,Integer>>(map.entrySet());
+		Collections.sort(listData,new Comparator<Map.Entry<Character,Integer>>(){
+
+			@Override
+			public int compare(Entry<Character, Integer> o1,
+					Entry<Character, Integer> o2) {
+				// TODO Auto-generated method stub
+				if(o2.getValue()!=null&&o1.getValue()!=null&&o2.getValue().compareTo(o1.getValue())>0){    
+	                return 1;    
+	               }else{    
+	                return -1;    
+	               }    
+			}
+			
+		});
+		System.out.println(listData.get(0));
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String character = "fsdkjfklsdjfksdlkjdsfoweiopriewfklsdkfldksaflkdsafldkfsieoirwpoiroewofkdsfksldkf";
+		Map<Character,Integer> map = new HashMap<Character, Integer>();
+		map = countChars(character);
+		SortMap(map);
+	}
+
+}
