80b9af2b5caf909f853516727475ebbc53c0f3dd
Bo Chen <chanber0314@gmail.com>
2017-07-31 19:01:52 +0800

    change data process

--- MklForReservoir/DataVisualize.py
+++ MklForReservoir/DataVisualize.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-import numpy as np
 import plotly.plotly
 import plotly.graph_objs as go
+import utility as util
 
-def visualize(testData, predictedData):
-    xList,yList,zList = dataTransMat(testData,predictedData)
+def visualize(xyData, predictedData):
+    xList,yList = util.getXYrange(xyData)
+    zList = util.dataTransMat(xyData, predictedData)
     layout = dict(width=600, height=800)
     trace = go.Heatmap(z= zList,
                        x= xList,
@@ -13,44 +14,4 @@ def visualize(testData, predictedData):
     figure = dict(data=[trace],layout=layout)
     plotly.offline.plot(figure)
 
-#（二维）将预测数据与坐标鼠标转为 plotly所需要的网格数据 x一维 y一维 z二维
-def dataTransMat(testData, zValue):
-    xData = testData[:, 0]
-    yData = testData[:, 1]
-    if np.size(xData) != np.size(yData):
-        return
-    zData = zValue
-    xyzList = [xData,yData]
-    xyzList.append(zData)
-    npXyzList = np.transpose(np.array(xyzList))
-    npXyzList= np.array(map(roundSix,npXyzList))
-    xList,yList = getXYrange(testData)
-    gridList = np.zeros((len(yList),len(xList)),dtype=float)
-    for xIndex,xValues in enumerate(xList):
-        for yIndex,yValues in enumerate(yList):
-            index = np.where((npXyzList[:,0] == xValues) * (npXyzList[:,1] == yValues))
-            temp = npXyzList[index]
-            if temp.size == 3:
-                gridList[yIndex, xIndex] = zData[index[0]]
-    return xList,yList,gridList.tolist()
-#得到x y坐标不重复排列
-def getXYrange(testData):
-    xData = testData[:, 0]
-    yData = testData[:, 1]
-    xList,yList = [],[]
-    # 根据xy最小最大坐标得到网格范围
-    xyList = np.array(map(roundSix, testData[:,0:2]))
-    xMin, xMax = np.min(xData), np.max(xData)
-    yMin, yMax = np.min(yData), np.max(yData)
-    for i in xyList:
-        if i[0] >= xMin and i[0] <= xMax and (i[0] in xList) == False:
-            xList.append(i[0])
-        if i[1] >= yMin and i[1] <= yMax and (i[1] in yList) == False:
-            yList.append(i[1])
-    xList.sort()
-    yList.sort()
-    return xList,yList
 
-def roundSix(x):
-    result = [round(i,6) for i in x]
-    return result
---
+++ MklForReservoir/utility.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+#（二维）将预测数据与坐标鼠标转为 plotly所需要的网格数据 x一维 y一维 z二维
+def dataTransMat(coordData, zValue):
+    xData = coordData[:, 0]
+    yData = coordData[:, 1]
+    if np.size(xData) != np.size(yData):
+        return
+    zData = zValue
+    xyzList = [xData,yData]
+    xyzList.append(zData)
+    npXyzList = np.transpose(np.array(xyzList))
+    npXyzList= np.array(map(roundSix,npXyzList))
+    xList,yList = getXYrange(coordData)
+    gridList = np.zeros((len(yList),len(xList)),dtype=float)
+    for xIndex,xValues in enumerate(xList):
+        for yIndex,yValues in enumerate(yList):
+            index = np.where((npXyzList[:,0] == xValues) * (npXyzList[:,1] == yValues))
+            temp = npXyzList[index]
+            if temp.size == 3:
+                gridList[yIndex, xIndex] = zData[index[0]]
+    return gridList.tolist()
+#得到x y坐标不重复排列
+def getXYrange(testData):
+    xData = testData[:, 0]
+    yData = testData[:, 1]
+    xList,yList = [],[]
+    # 根据xy最小最大坐标得到网格范围
+    xyList = np.array(map(roundSix, testData[:,0:2]))
+    xMin, xMax = np.min(xData), np.max(xData)
+    yMin, yMax = np.min(yData), np.max(yData)
+    for i in xyList:
+        if i[0] >= xMin and i[0] <= xMax and (i[0] in xList) == False:
+            xList.append(i[0])
+        if i[1] >= yMin and i[1] <= yMax and (i[1] in yList) == False:
+            yList.append(i[1])
+    xList.sort()
+    yList.sort()
+    return xList,yList
+
+def roundSix(x):
+    result = [round(i,6) for i in x]
+    return result
